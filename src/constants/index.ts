dc895895cb9275ab05eb567af39b5ecc28fe7603
seiya_tagami <bepozurapu274@gmail.com>
2023-06-03 12:33:13 +0900

    chore: adding files and folder

---
+++ src/constants/index.ts
@@ -0,0 +1,88 @@
+const OPTIONS_DATA = [
+  {
+    id: "1",
+    label: "Flip machine",
+    desc: "¥25 / click",
+    maxPurchases: 500,
+    price: 15000,
+    img: "images/grill.png"
+  },
+  {
+    id: "2",
+    label: "ETF Stock",
+    desc: "0.1% per second on total purchases",
+    maxPurchases: Infinity,
+    price: 300000,
+  },
+  {
+    id: "3",
+    label: "ETF Bonds",
+    desc: "0.07% per second on total purchases",
+    maxPurchases: Infinity,
+    price: 300000,
+  },
+  {
+    id: "4",
+    label: "Lemonade Stand",
+    desc: "¥30 / second",
+    maxPurchases: 1000,
+    price: 30000,
+    img: "images/lemonade.png"
+  },
+  {
+    id: "5",
+    label: "Ice Cream Truck",
+    desc: "¥120 / second",
+    maxPurchases: 500,
+    price: 100000,
+    img: "images/ice-cream.png"
+  },
+  {
+    id: "6",
+    label: "House",
+    desc: "¥32,000 / second",
+    maxPurchases: 100,
+    price: 20000000,
+    img: "images/house-1.png"
+  },
+  {
+    id: "7",
+    label: "TownHouse",
+    desc: "¥64,000 / second",
+    maxPurchases: 100,
+    price: 40000000,
+    img: "images/house-2.png"
+  },
+  {
+    id: "8",
+    label: "Castle",
+    desc: "¥500,000 / second",
+    maxPurchases: 20,
+    price: 250000000,
+    img: "images/castle.png"
+  },
+  {
+    id: "9",
+    label: "Industrial Space",
+    desc: "¥2,200,000 / second",
+    maxPurchases: 10,
+    price: 1000000000,
+    img: "images/industrial-space.png"
+  },
+  {
+    id: "10",
+    label: "Hotel Skyscraper",
+    desc: "¥25,000,000 / second",
+    maxPurchases: 5,
+    price: 10000000000,
+    img: "images/skyscrapers.png"
+  },
+  {
+    id: "11",
+    label: "Bullet-Speed Sky Railway",
+    desc: "¥30,000,000,000 / second",
+    maxPurchases: 1,
+    price: 10000000000000,
+    img: "images/high-speed-train.png"
+  },
+];
